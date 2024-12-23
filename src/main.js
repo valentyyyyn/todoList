f2d69751a06aadb258d35b966a8f50584d567ac0
valentyyyyn <gomezvalentinmiguel@gmail.com>
2024-12-23 18:54:39 -0300

    minimal changes

---
+++ src/main.js
@@ -0,0 +1,4 @@
+const taskInput = document.getElementById('task_input');
+const addTaskButton = document.getElementById('add_task_button');
+const taskList = document.getElementById('task_list');
+const noPendingTask = document.getElementById('no_pending_task_message');
